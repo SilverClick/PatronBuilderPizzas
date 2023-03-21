ab261919f23bb5fae2adb37964718446b4b1d0df
Gonzalo Arca Iglesias <garcaiglesias@danielcastelao.org>
2023-03-21 09:40:51 +0100

    primeraparte

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Pizzas pizzaSinCebolla=new Pizzas("Normal",false,2,true,"Solo Tomate",false,false,false,false,true,true);
+        System.out.println(pizzaSinCebolla.toString());
+        Pizzas pizzaConPiña=new Pizzas("Normal",false,2,true,"Solo Tomate",true,false,false,true,true,true);
+        System.out.println(pizzaConPiña.toString());
+        Pizzas pizzaSinGluten=new Pizzas("Normal",false,2,true,"Sin Gluten",false,true,false,false,true,true);
     }
 }
--- src/Pizzas.java
+++ src/Pizzas.java
@@ -1,16 +1,74 @@
 public class Pizzas {
-private String tipoMasa;
 
-public final String normal="Normal";
-public final String integral="Integral";
-public final String fina="Fina";
-public final String masaPan="Masa de Pan";
+    private String tipoMasa;
 
-private boolean relleno;
+    private boolean relleno;
 
-private int tamaño;
+    private int tamaño;
 
-public final int familiar=1;
-public final int mediana=2;
-public final int pequeña=3;
+    private final int familiar = 1;
+    private final int mediana = 2;
+    private final int pequeña = 3;
+
+    private boolean salsa;
+
+    private String tipoSalsa;
+
+    private boolean cebolla;
+
+    private boolean sinGluten;
+
+    private boolean extraQueso;
+
+    private boolean piña;
+
+    private boolean champiñones;
+
+    private boolean jamon;
+    /**
+     * Constructor con todoa los parametros
+     * @param tipoMasa espesor de la masa
+     * @param relleno si esta rellena la pizza
+     * @param tamaño el tamaño de la pizza
+     * @param salsa si lleva salsa
+     * @param tipoSalsa que tipo de salsa lleva
+     * @param cebolla lleva o no lleva
+     * @param sinGluten para celíacos
+     * @param extraQueso si tiene extra de queso la pizza
+     * @param piña si lleva piña la pizza
+     * @param champiñones si lleva champiñones la pizza.
+     * @param jamon si lleva jamon la pizza
+     */
+
+    public Pizzas(String tipoMasa, boolean relleno, int tamaño, boolean salsa, String tipoSalsa, boolean cebolla, boolean sinGluten, boolean extraQueso, boolean piña, boolean champiñones, boolean jamon) {
+        this.tipoMasa = tipoMasa;
+        this.relleno = relleno;
+        this.tamaño = tamaño;
+        this.salsa = salsa;
+        this.tipoSalsa = tipoSalsa;
+        this.cebolla = cebolla;
+        this.sinGluten = sinGluten;
+        this.extraQueso = extraQueso;
+        this.piña = piña;
+        this.champiñones = champiñones;
+        this.jamon = jamon;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "tipoMasa='" + tipoMasa + '\'' +
+                ", relleno=" + relleno +
+                ", tamaño=" + tamaño +
+                ", salsa=" + salsa +
+                ", tipoSalsa='" + tipoSalsa + '\'' +
+
+                ", cebolla=" + cebolla +
+                ", sinGluten=" + sinGluten +
+                ", extraQueso=" + extraQueso +
+                ", piña=" + piña +
+                ", champiñones=" + champiñones +
+                ", jamon=" + jamon
+               ;
+    }
 }
